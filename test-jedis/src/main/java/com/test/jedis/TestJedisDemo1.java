623a9c828db1e5585fa662bb4d484505ec350d5c
H__D <naughty7878@163.com>
2018-07-05 18:23:38 +0800

    jedis学习

---
+++ test-jedis/src/main/java/com/test/jedis/TestJedisDemo1.java
@@ -0,0 +1,51 @@
+package com.test.jedis;
+
+import org.junit.Test;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+public class TestJedisDemo1 {
+
+	/**
+	 * 单实例连接redis数据库
+	 * @Description TODO
+	 * @author H__D
+	 * @date 2018年7月5日
+	 */
+	@Test
+	public void run1() {
+		
+		Jedis jedis = new Jedis("120.78.189.168", 6379);
+		jedis.set("sex", "男");
+		System.out.println(jedis.get("sex"));
+	}
+
+	/**
+	 * Jedis连接池
+	 * @Description TODO
+	 * @author H__D
+	 * @date 2018年7月5日
+	 */
+	@Test
+	public void run2() {
+		
+		// 1、设置连接池的配置对象
+		JedisPoolConfig config = new JedisPoolConfig();
+		// 设置池中最大的连接数量（可选）
+		config.setMaxTotal(50);
+		// 设置空闲时池中保有的最大连接数（可选）
+		config.setMaxIdle(10);
+		
+		// 2、设置连接池对象
+		JedisPool pool = new JedisPool(config, "120.78.189.168", 6379);
+		
+		// 3、从池中获取连接对象
+		Jedis jedis = pool.getResource();
+		System.out.println(jedis.get("sex"));
+		
+		// 4、连接池归还
+		jedis.close();
+	}
+}
