f59e07e57eace77507544ec70d60ea65595fd005
god <naughty7878@163.com>
2020-06-06 23:13:17 +0800

    test-springboot-admin-client

---
+++ test-springboot-admin-client/src/main/java/com/test/springboot/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.test.springboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping(value = "/hello")
+    public String hello(){
+        return "hello world";
+    }
+}
