8415373a724bdf09003d6d1ae182b940290d17a6
god <naughty7878@163.com>
2020-06-07 12:28:24 +0800

    test-mvn-package

---
+++ test-mvn-package/src/main/java/com/test/api/MyMain.java
@@ -0,0 +1,16 @@
+package com.test.api;
+
+import cn.hutool.core.date.DateUtil;
+
+public class MyMain {
+
+    public static void main(String[] args) {
+        System.out.println("===========程 序 启 动===========");
+
+        //当前时间字符串，格式：yyyy-MM-dd HH:mm:ss
+        String now = DateUtil.now();
+        System.out.println("当前时间：" + now);
+
+        System.out.println("===========程 序 关 闭===========");
+    }
+}
