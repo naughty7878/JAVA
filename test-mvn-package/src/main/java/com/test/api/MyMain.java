cd145c756e4bcb57702c10c3fe3139ef1bd8f535
god <naughty7878@163.com>
2020-06-07 13:29:04 +0800

    test-mvn-package

--- test-mvn-package/src/main/java/com/test/api/MyMain.java
+++ test-mvn-package/src/main/java/com/test/api/MyMain.java
@@ -7,7 +7,8 @@ public class MyMain {
     public static void main(String[] args) {
         System.out.println("===========程 序 启 动===========");
 
-        //当前时间字符串，格式：yyyy-MM-dd HH:mm:ss
+        // 当前时间字符串，格式：yyyy-MM-dd HH:mm:ss
+        // DateUtil 第三方jar包中的类
         String now = DateUtil.now();
         System.out.println("当前时间：" + now);
 
