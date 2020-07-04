540c75bcb996b516e409a5674e06ae358f6454be
god <naughty7878@163.com>
2020-07-04 23:25:51 +0800

    20200704

--- test-springboot-admin-sc-server/src/main/java/com/test/springboot/Application.java
+++ test-springboot-admin-sc-server/src/main/java/com/test/springboot/Application.java
@@ -4,6 +4,7 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.util.StringUtils;
 
 @EnableDiscoveryClient
 @EnableAdminServer
