ba125bedadfc969591e66c6c0c58c4aa22ced3b4
god <naughty7878@163.com>
2021-02-25 11:25:57 +0800

    java反射：动态代理

--- test-reflect/src/main/java/com/test/java2/MethodTest.java
+++ test-reflect/src/main/java/com/test/java2/MethodTest.java
@@ -89,7 +89,5 @@ public class MethodTest {
             System.out.println();
         }
 
-
-
     }
 }
--- test-reflect/src/main/java/com/test/java2/OtherTest.java
+++ test-reflect/src/main/java/com/test/java2/OtherTest.java
@@ -14,7 +14,6 @@ public class OtherTest {
 
     /*
     获取构造器结构
-
      */
     @Test
     public void test1(){
