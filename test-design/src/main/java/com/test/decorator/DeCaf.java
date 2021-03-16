5c062185195dbe0782aebf75055d3c71fe35b117
god <naughty7878@163.com>
2021-03-16 17:43:11 +0800

    设计模式之装饰者模式

---
+++ test-design/src/main/java/com/test/decorator/DeCaf.java
@@ -0,0 +1,10 @@
+package com.test.decorator;
+
+public class DeCaf extends Coffee {
+
+	public DeCaf() {
+		setDes(" 无因咖啡 ");
+		setPrice(1.0f);
+	}
+
+}
---
+++ test-design/src/main/java/com/test/uml/Worker.java
@@ -0,0 +1,7 @@
+package com.test.uml;
+
+public interface Worker {
+    public void worker1();
+    public void worker2();
+    public void worker3();
+}
