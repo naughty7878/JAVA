fdc3eaea07e1c1c4f69120aa41ed7348da6c2118
god <naughty7878@163.com>
2021-03-28 23:58:11 +0800

    设计模式-模版方法模式

---
+++ test-design/src/main/java/com/test/template/TemplateDemo.java
@@ -0,0 +1,62 @@
+package com.test.template;
+
+public class TemplateDemo {
+    public static void main(String[] args) {
+        // 制作红豆豆浆
+        System.out.println("----制作红豆豆浆----");
+        SoyaMilk redBeanSoyaMilk = new RedBeanSoyaMilk();
+        redBeanSoyaMilk.make();
+        System.out.println("----制作花生豆浆----");
+        SoyaMilk peanutSoyaMilk = new PeanutSoyaMilk();
+        peanutSoyaMilk.make();
+    }
+}
+
+// 抽象类，表示豆浆
+abstract class SoyaMilk {
+
+    // 模板方法，make，模板方法可以做成 final，不让子类去覆盖
+    final void make() {
+        select();
+        addCondiments();
+        soak();
+        beat();
+    }
+
+    // 选材料
+    void select() {
+        System.out.println("第一步：选择好的新鲜黄豆 ");
+    }
+
+    // 添加不同的配料，抽象方法，子类具体实现
+    abstract void addCondiments();
+
+    // 浸泡
+    void soak() {
+        System.out.println("第三步，黄豆和配料开始浸泡，需要 3 小时 ");
+    }
+
+    // 打碎
+    void beat() {
+        System.out.println("第四步：黄豆和配料放到豆浆机去打碎 ");
+    }
+}
+
+
+// 花生豆浆
+class PeanutSoyaMilk extends SoyaMilk {
+
+    @Override
+    void addCondiments() {
+        System.out.println(" 加入上好的花生 ");
+    }
+}
+
+// 红豆豆浆
+class RedBeanSoyaMilk extends SoyaMilk {
+
+    @Override
+    void addCondiments() {
+        System.out.println(" 加入上好的红豆 ");
+    }
+}
