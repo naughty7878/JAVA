c8e15f71c8e06db672df3151375d0a60cf376ef8
god <naughty7878@163.com>
2021-05-15 11:40:18 +0800

    leetcode 初级算法

---
+++ test-leetcode/src/main/java/com/level1/other/HammingDistance.java
@@ -0,0 +1,47 @@
+package com.level1.other;
+
+/**
+ * 汉明距离
+ * 两个整数之间的汉明距离指的是这两个数字对应二进制位不同的位置的数目。
+ *
+ * 给出两个整数 x 和 y，计算它们之间的汉明距离。
+ *
+ * 注意：
+ * 0 ≤ x, y < 231.
+ *
+ * 示例:
+ *
+ * 输入: x = 1, y = 4
+ *
+ * 输出: 2
+ *
+ * 解释:
+ * 1   (0 0 0 1)
+ * 4   (0 1 0 0)
+ *        ↑   ↑
+ *
+ * 上面的箭头指出了对应二进制位不同的位置。
+ *
+ * 作者：力扣 (LeetCode)
+ * 链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnyode/
+ * 来源：力扣（LeetCode）
+ * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
+public class HammingDistance {
+
+    public int hammingDistance(int x, int y) {
+        int count = 0;
+        for (int i = 0; i < 32; i++) {
+            if (((x >> i) & 1) !=  ((y >> i) & 1)) {
+//                System.out.println(i);
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int hammingDistance = new HammingDistance().hammingDistance(1, 4);
+        System.out.println(hammingDistance);
+    }
+}
---
+++ test-leetcode/src/main/java/com/level1/tree/LevelOrder.java
@@ -0,0 +1,107 @@
+package com.level1.tree;
+
+import java.util.*;
+
+public class LevelOrder {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ret = new ArrayList<>();
+        if (root == null) {
+            return ret;
+        }
+
+        // 利用队列
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
+            int currentSize = queue.size();
+            for (int i = 0; i < currentSize; i++) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            ret.add(list);
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        // 给定二叉树 [3,9,20,null,null,15,7]
+        TreeNode node = new TreeNode(3);
+        node.left = new TreeNode(9);
+        node.right = new TreeNode(20);
+        node.right.left = new TreeNode(15);
+        node.right.right = new TreeNode(7);
+        List<List<Integer>> lists = new LevelOrder().levelOrder(node);
+        lists.stream().forEach(
+                System.out::println
+        );
+    }
+
+    // 方法1
+    public List<List<Integer>> levelOrder1(TreeNode root) {
+        List<List<Integer>> rootList = new ArrayList<>();
+        List<TreeNode> treeNodes = levelList(rootList, Arrays.asList(root));
+        while (treeNodes.size() > 0) {
+            treeNodes  = levelList(rootList, treeNodes);
+        }
+        return rootList;
+    }
+
+    private List<TreeNode> levelList(List<List<Integer>> rootList, List<TreeNode> treeNodes) {
+        List<Integer> list = new ArrayList<>();
+        List<TreeNode> nodeList = new ArrayList<>();
+        for (TreeNode node: treeNodes) {
+            if(node == null) {
+                continue;
+            }
+            list.add(node.val);
+            nodeList.add(node.left);
+            nodeList.add(node.right);
+        }
+        if(list.size() > 0) {
+            rootList.add(list);
+        }
+        return nodeList;
+    }
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
